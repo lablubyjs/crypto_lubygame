61b24060d7fae2626da0de54f9f9a253a5b95f66
Crislâine Santos <criis.santos@outlook.com>
2022-06-02 19:44:56 -0300

    fix: component fixes added

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -28,4 +28,8 @@ export const ButtonContainer = styled.button<IButtonProps>`
         opacity: 0.85;
         box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.4);
     }
+
+    &:disabled {
+        cursor: not-allowed;
+    }
 `;
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,11 +1,15 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+import Button from '../Button';
+import Mascot from '../Mascot';
+import Loading from '../Loading';
+
 import {GameProps} from '@shared/types';
 
-import * as S from './styles';
 import {theme, Text} from '@shared/styles';
-import Button from '../Button';
-import {useNavigate} from 'react-router-dom';
-import Mascot from '../Mascot';
+
+import * as S from './styles';
 
 const Game: React.FC<GameProps> = ({
     categorie,
@@ -29,7 +33,7 @@ const Game: React.FC<GameProps> = ({
     };
 
     if (!questions) {
-        return <p>Loading...</p>;
+        return <Loading />;
     }
 
     if (currentQuestion > questions.length - 1) {
@@ -43,7 +47,7 @@ const Game: React.FC<GameProps> = ({
                     perguntas.
                 </Text>
                 <Button
-                    width={10}
+                    width={13}
                     height={3}
                     backgroundColor={theme.colors.white}
                     borderColor={categorie.color}
@@ -52,7 +56,7 @@ const Game: React.FC<GameProps> = ({
                     onClick={() => navigate('/lubygame')}
                 >
                     <Text fontSize={1} fontWeight="600" color={categorie.color}>
-                        Voltar
+                        Jogar Novavente
                     </Text>
                 </Button>
                 <Mascot />
@@ -85,21 +89,22 @@ const Game: React.FC<GameProps> = ({
                 <Text fontSize={1.3} fontWeight="600" color={categorie.color}>
                     {questions[currentQuestion].question}
                 </Text>
-                {questions[currentQuestion].answers.map((answer, index) => (
-                    <S.Option key={answer} color={categorie.color}>
-                        <div className="radio">
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="answer"
-                                    value={index}
-                                    onChange={() => setAnswer(index)}
-                                />
-                                <span>{answer}</span>
-                            </label>
-                        </div>
-                    </S.Option>
-                ))}
+                {!feedback &&
+                    questions[currentQuestion].answers.map((answer, index) => (
+                        <S.Option key={answer} color={categorie.color}>
+                            <div className="radio">
+                                <label>
+                                    <input
+                                        type="radio"
+                                        name="answer"
+                                        value={index}
+                                        onChange={() => setAnswer(index)}
+                                    />
+                                    <span>{answer}</span>
+                                </label>
+                            </div>
+                        </S.Option>
+                    ))}
                 {feedback && (
                     <>
                         <Text
@@ -148,23 +153,27 @@ const Game: React.FC<GameProps> = ({
                         fontWeight="600"
                         color={theme.colors.white}
                     >
-                        Próxima Pergunta
+                        {currentQuestion === questions.length - 1
+                            ? `Terminar Jogo`
+                            : `Próxima Pergunta`}
                     </Text>
                 </Button>
             )}
-            <Button
-                width={10}
-                height={3}
-                backgroundColor={theme.colors.white}
-                borderColor={categorie.color}
-                borderWidth={2}
-                borderRadius={50}
-                onClick={goBackHandler}
-            >
-                <Text fontSize={1} fontWeight="600" color={categorie.color}>
-                    Voltar
-                </Text>
-            </Button>
+            {!feedback && currentQuestion < questions.length - 1 && (
+                <Button
+                    width={10}
+                    height={3}
+                    backgroundColor={theme.colors.white}
+                    borderColor={categorie.color}
+                    borderWidth={2}
+                    borderRadius={50}
+                    onClick={goBackHandler}
+                >
+                    <Text fontSize={1} fontWeight="600" color={categorie.color}>
+                        Voltar para categorias
+                    </Text>
+                </Button>
+            )}
         </S.GameContainer>
     );
 };
