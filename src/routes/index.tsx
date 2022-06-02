fb56ac1ad8547ea750d9c1a3884fbad9c73c3342
Crislâine Santos <criis.santos@outlook.com>
2022-06-02 13:29:13 -0300

    feat: categorie route added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
 
-import {Home, LubyGame} from '@pages';
+import {Questions, Home, LubyGame} from '@pages';
 
 const RoutesContainer: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/lubygame" element={<LubyGame />} />
+            <Route path="/lubygame/:categorie/*" element={<Questions />} />
         </Routes>
     );
 };
