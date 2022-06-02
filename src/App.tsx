708e488f6262a1574e883ba511086f1f97092d12
Crislâine Santos <criis.santos@outlook.com>
2022-06-02 20:26:19 -0300

    feat: add routes container, store provider and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
 import {BrowserRouter} from 'react-router-dom';
+
+import {ThemeProvider} from 'styled-components';
+
+import {useAppDispatch} from '@hooks';
 
 import RoutesContainer from '@routes';
 
+import {asyncAddQuestions} from '@store/questions-slice';
+import {asyncFetchConnection} from '@store/connection-slice';
+
+import {GlobalStyle, theme} from '@shared/styles';
+
 function App() {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await dispatch(asyncAddQuestions());
+        };
+
+        fetchData();
+    }, [dispatch]);
+
+    useEffect(() => {
+        const fetchConnection = async () => {
+            await dispatch(asyncFetchConnection());
+        };
+
+        fetchConnection();
+    }, [dispatch]);
+
     return (
-        <BrowserRouter>
-            <RoutesContainer />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <GlobalStyle />
+                <RoutesContainer />
+            </BrowserRouter>
+        </ThemeProvider>
     );
 }
 
